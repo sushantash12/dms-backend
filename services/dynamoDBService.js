3f2b3f7f103aff30c2ef6857108a67326b5ab6d1
sushantash12 <122538597+sushantash12@users.noreply.github.com>
2023-11-19 19:09:32 -0600

    Update dynamoDBService.js

--- services/dynamoDBService.js
+++ services/dynamoDBService.js
@@ -3,8 +3,8 @@ const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const dynamoDBClient  = new DynamoDBClient({
     region: 'us-east-1',
     credentials: {
-        accessKeyId: 'AKIA6KYON4BHWF7WPUG3',
-        secretAccessKey: '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'
+        accessKeyId: 'xxxxxx',
+        secretAccessKey: 'xxxxxx'
     }
 });
 
